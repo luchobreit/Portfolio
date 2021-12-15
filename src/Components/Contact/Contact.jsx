d8ccb1ac8c937a9c4f85b0c75e189efc48da7daf
luchobreit <luciobreit@gmail.com>
2021-12-14 23:03:33 -0300

    primer subida a hosting

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,7 +1,25 @@
 import "./contact.scss"
+import { useState, useRef } from "react";
+import emailjs from 'emailjs-com';
+
 
 function Contact() {
+    const [complete, setComplete] = useState(false)
+    const form = useRef();  
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_r8xzly2', 'template_nqwlnj5', form.current, 'user_owJYa9C0g94ladlw9mtK7')
+          .then((result) => {
+             alert(result.text);
+              setComplete(true)
+          }, (error) => {
+              console.log(error.text);
+          });
+        e.target.reset()
+      };
     return (
+        
         <div id="contact" className="contact">
             <div className="left-c">
                 <h1>Contacto</h1>
@@ -9,13 +27,24 @@ function Contact() {
             </div>
 
             <div className="right-c">
-                <form className="form-c">
-                    <input autoComplete="off" type="mail" name="mail" placeholder="Mail" className="input mail" />
-                    <input autoComplete="off" type="text" name="nombre" placeholder="Nombre" className="input mail" />
-                    <textarea className="texto" name="text" placeholder="Mensaje"></textarea>
+                <form ref={form} onSubmit={sendEmail}className="form-c">
+                    <input autoComplete="off" required type="mail" name="mail" placeholder="Mail" className="input mail" />
+                    <input autoComplete="off" required type="text" name="nombre" placeholder="Nombre" className="input mail" />
+                    <textarea className="texto" required name="text" placeholder="Mensaje"/>
                     
-                    <button className="button" type="button" > Enviar </button>
+                    <button className="button" > Enviar </button>
+                    {complete ?
+                        <h3>Gracias respondere lo más rapido posible</h3>
+                        :
+                        <></>
+                    }
                 </form>
+                <div className="redes-container">
+                <a href="https://www.instagram.com/luchobreit_/" target="_blank" className="link-c"><img src="/img/RRSS/instagram2.png" alt="ig" className="rrss ig" ></img></a>
+                <a target="_blank" href="https://wa.me/5491150550571" className="link-c"><img src="/img/RRSS/wpp2.png" alt="wpp" className="rrss wpp" ></img></a>
+                <a target="_blank" href="https://www.linkedin.com/in/lucio-breitbart/" className="link-c"><img src="/img/RRSS/in.png" alt="in" className="rrss in" ></img></a>
+                </div>
+
             </div>
         </div>
     )
